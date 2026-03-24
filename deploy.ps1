9b171190b3680fe198489da3281cfa2b78278d13
xsid1 <xsid1@users.noreply.github.com>
2026-03-24 17:12:42 +0900

    ✨ 최종 배포: 키 경로 변수 순서 수정 및 배포 재시도

--- deploy.ps1
+++ deploy.ps1
@@ -1,8 +1,8 @@
 # AdMaker 4.0 One-Click Deployment Script (PowerShell)
 
 $IP = "13.125.184.180"
-$KEY_PATH = "$LOCAL_DIR\LightsailDefaultKey-ap-northeast-2.pem"
 $LOCAL_DIR = "C:\Users\xsid1\Desktop\AdMaker4.0"
+$KEY_PATH = "$LOCAL_DIR\LightsailDefaultKey-ap-northeast-2.pem"
 $REMOTE_USER = "ubuntu"
 $REMOTE_PATH = "~/AdMaker4.0"
 
